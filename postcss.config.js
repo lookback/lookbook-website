7f6c85a41f46984d4c1247ab31a4e817e8955e4a
Johan Brook <johan@lookback.io>
2019-06-11 16:40:24 +0200

    Fix mobile design

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
 const { defaultPostCssPlugins } = require('lookbook');
+const postcssCustomMedia = require('postcss-custom-media');
 
 module.exports = (ctx) => ({
     map: ctx.options.map, // Sourcemaps
     plugins: [
         ...defaultPostCssPlugins(),
+        postcssCustomMedia({
+            preserve: true,
+        }),
         // TODO Minify
     ],
 });
