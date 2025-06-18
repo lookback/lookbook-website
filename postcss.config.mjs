d270e153f23944c6ad86aa3660f616171f5e4d8e
Johan Brook <johan@lookback.io>
2025-06-18 10:36:13 +0200

    Tailwind v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,5 @@
-import { defaultPostCssPlugins } from '@lookback/lookbook';
-import minify from 'postcss-csso';
-
-export default function (ctx) {
-    return {
-        map: ctx.options.map, // Sourcemaps
-        plugins: [
-            ...defaultPostCssPlugins(),
-            ctx.env === 'production' ? minify : null,
-        ],
-    };
-}
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
